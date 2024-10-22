7d1349f91b5f2026ea28748d0a5acaaf160c0a4b
i_Zolox <33046239+izolox@users.noreply.github.com>
2024-10-21 20:37:35 -0500

    refactror(index): remove app.ts and update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import { initialize } from "./app";
+import express, { Application } from "express";
+import * as dotenv from "dotenv";
+import SpeciesGrabber from "./classes/species-grabber";
+import fs from "fs";
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
-initialize();
+dotenv.config();
+
+const Grabber = new SpeciesGrabber();
+const app = express();
+
+app.get("/creatures/:name", (req: express.Request, res: express.Response) => {
+    const { name } = req.params;
+    Grabber.fetchSpecies(name)
+        .then((data) => res.json(data))
+        .catch((error) => res.status(500).json({ error: error.message }));
+});
+
+export default app;
